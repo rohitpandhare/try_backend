0ed775767b510b0b5cdd35d5abcdaec551aa15f6
Rohit Pandhare <93035996+rohitpandhare@users.noreply.github.com>
2025-01-01 21:05:17 +0530

    Update ik_downloader.py

--- ik_downloader.py
+++ ik_downloader.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 class IndianKanoonDownloader:
     def __init__(self):
-        self.api_key = b2dd13cd3162e7ecc7eba6e9636fdd33fe8755b4
+        self.api_key = "b2dd13cd3162e7ecc7eba6e9636fdd33fe8755b4"
         self.base_url = "https://api.indiankanoon.org"
         self.headers = {
             'Authorization': f"Token {self.api_key}",
